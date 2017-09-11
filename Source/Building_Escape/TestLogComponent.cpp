dce03e9956a0cef933ddf838486361d253c934e5
mrr0088 <mrr0088@alu.ubu.es>
2017-09-11 02:54:42 +0200

    Level Upgraded

--- Source/Building_Escape/TestLogComponent.cpp
+++ Source/Building_Escape/TestLogComponent.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "TestLogComponent.h"
+
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UTestLogComponent::UTestLogComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,18 +15,16 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UTestLogComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s position is %s"), *ObjectName, *ObjectPos);
 
+	UE_LOG(LogTemp, Warning, TEXT("Your message related to the couch"));
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UTestLogComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
